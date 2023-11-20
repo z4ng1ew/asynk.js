6b67424d82a2feaf0d6ad71a46e2fc046c700a43
z4ng1ew <z4ng1ew@gmail.com>
2023-11-21 01:02:20 +0300

    Initial commit for async_js branch

---
+++ async.js
@@ -0,0 +1,55 @@
+
+//ЗАДАЧА № 1
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts/1/comments')
+//   .then(value => {
+//     return value.json();
+//   })
+//   .then(value => {
+//     const evenIds = value.filter(item => item.id % 2 === 0);
+//     console.log(evenIds);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   });
+
+
+
+
+//ЗАДАЧА № 2
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/albums/1/photos')
+// .then(value => {
+//     return value.json();
+// })
+// .then(value =>{
+//     const eventitles = value.filter (item => item.title.charAt(0) === "a")
+//    console.log(eventitles);
+// })
+// .catch(error => {
+//         console.log(error);
+//       });
+
+
+
+//ЗАДАЧА № 3
+
+
+
+  //     fetch('https://jsonplaceholder.typicode.com/users/1/albums')
+  // .then(value => {
+  //   return value.json();
+  // })
+  
+  // .then(value => {
+  //   const evenIds = value.filter(item => item.id > 5);
+  //   console.log(evenIds);
+  // })
+  // .catch(error => {
+  //   console.log(error);
+  // });
+
+
